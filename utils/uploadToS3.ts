cc72e46180aa6d45d4269c13fc3631177eef8802
kartik126 <lavi9921@gmail.com>
2023-12-20 11:56:07 +0530

    chore:increase upload size in s3 bucket

--- utils/uploadToS3.ts
+++ utils/uploadToS3.ts
@@ -8,9 +8,19 @@ export const uploadToS3 = async (email: string, imageBuffer: Buffer) => {
       Body: imageBuffer,
       ContentType: "image/jpeg",
       ACL: "public-read",
+      PartSize: 10 * 1024 * 1024, // 10 MB part size (adjust as needed)
+      httpOptions: {
+        timeout: 600000, // 10 minutes timeout (adjust as needed)
+      },
     };
 
-    const s3UploadResult = await s3.upload(s3UploadParams).promise();
+    const s3UploadResult = await s3
+      .upload(s3UploadParams)
+      .on("httpUploadProgress", (progress:any) => {
+        console.log("upload progress...",progress)
+      })
+      .promise();
+
     return s3UploadResult.Location;
   } catch (error) {
     throw error;
